d45e7997578951941716588d03c36b483d5d8aca
aneeshchali <chali.aneesh@gmail.com>
2023-01-07 09:40:52 +0530

    started redux

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
-import React, { useState } from "react";
+import Counter from './components/Counter';
 
-import Header from "./components/Layout/Header";
-import Meals from "./components/Meals/Meals";
-import Cart from "./components/Cart/Cart";
-import CartProvider from "./store/CartProvider";
 
-const App = () => {
-  const [IsCartShown, setIsCartShown] = useState(false);
-
-  const showCartHandler = () => {
-    setIsCartShown(true);
-  };
-  const hideCartHandler = () => {
-    setIsCartShown(false);
-  };
-
+function App() {
   return (
-    <CartProvider>
-      {IsCartShown && <Cart CloseCart={hideCartHandler} />}
-      <Header ShowCart={showCartHandler}></Header>
-      <main>
-        <Meals></Meals>
-      </main>
-    </CartProvider>
+    <Counter />
   );
-};
+}
 
 export default App;
