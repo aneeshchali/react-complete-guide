0f774bf88543b78e4d46664d57fcc543d385f484
aneeshchali <chali.aneesh@gmail.com>
2023-01-09 19:55:14 +0530

    completed with a bug

--- src/store/redux.js
+++ src/store/redux.js
@@ -1,16 +1,33 @@
-import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 
-const counterReducer = (state = { counter: 0 }, action) => {
-  if (action.type === "increment") {
-    return { counter: state.counter + 1 };
-  }
+import counterReducer from "./counterSlice";
+import authReducer from "./authSlice.";
 
-  if (action.type === "decrement") {
-    return { counter: state.counter - 1 };
-  }
-  return state;
-};
-
-const store = createStore(counterReducer);
-
+const store = configureStore({
+  reducer: { counter: counterReducer, auth: authReducer },
+});
 export default store;
+
+// const counterReducer = (state = initialState, action) => {
+//   if (action.type === "increment") {
+//     return { counter: state.counter + 1, showCounter: state.showCounter };
+//   }
+//   if (action.type === "toggle") {
+//     return { counter: state.counter, showCounter: !state.showCounter };
+//   }
+//   if (action.type === "increase") {
+//     return {
+//       counter: state.counter + action.amount,
+//       showCounter: state.showCounter,
+//     };
+//   }
+//   if (action.type === "decrement") {
+//     return { counter: state.counter - 1, showCounter: state.showCounter };
+//   }
+//   return state;
+// };
+
+// const store = createStore(counterSlice.reducer);
+
+// export const counterActions = counterSlice.actions;
+// export const authActions = authSlice.actions;
